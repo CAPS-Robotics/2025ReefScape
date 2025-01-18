cd79b488749ad33cf1aba4d3c9f49c05c5b75c3e
DTM-520 <157851788+DTM-520@users.noreply.github.com>
2025-01-18 15:05:02 -0600

    SwerveDrive work on sunday

--- FRC2025/src/main/java/frc/robot/Swerve/SwerveModule.java
+++ FRC2025/src/main/java/frc/robot/Swerve/SwerveModule.java
@@ -5,30 +5,53 @@
 package frc.robot.Swerve;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-
+import edu.wpi.first.wpilibj.AnalogEncoder;
 import com.revrobotics.spark.SparkFlex;
+import frc.robot.subsystems.*;
 
 
 /** Add your docs here. */
 public class SwerveModule {
 
-    private SparkMax steeringMotor;
-    private SparkFlex driveMotor;
+    public SparkMax steeringMotor;
+    public SparkFlex driveMotor;
     private SwerveModuleState moduleState;
+    public AnalogEncoder encoder;
+    public PIDController pidController; 
 
-    public SwerveModule(int driveMotorPort,int steeringMotorPort, double MetersPerSecond, Rotation2d angle ){
+ 
 
+    public SwerveModule(int driveMotorPort,int steeringMotorPort){
         steeringMotor = new SparkMax(steeringMotorPort, MotorType.kBrushless);
         driveMotor = new SparkFlex(driveMotorPort, MotorType.kBrushless);
 
-         moduleState = new SwerveModuleState(MetersPerSecond, angle);
+        moduleState = new SwerveModuleState();
 
-    }
+        pidController = new PIDController(0.03, 0, 0);
+
+
+        };
+
+    
 
     public SwerveModuleState getModuleState(){
         return moduleState;
+    }
+ 
+    public void setModuleState(SwerveModuleState state){
+        moduleState = state;
+
+        driveMotor.set(moduleState.speedMetersPerSecond);
+        double encoderValue = SwerveDriveTrainSubsystem.frontLeftEncoder.get();
+        double endpoint = moduleState.angle.getRadians()/(Math.PI*2);
+        System.out.println("Endpoint: "+endpoint);
+        steeringMotor.set(pidController.calculate(encoderValue, endpoint));
+       
+        
     }
 
 }
--- FRC2025/src/main/java/frc/robot/subsystems/SwerveDriveTrainSubsystem.java
+++ FRC2025/src/main/java/frc/robot/subsystems/SwerveDriveTrainSubsystem.java
@@ -4,27 +4,48 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.Kinematics;
+
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.*;
+import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.Swerve.SwerveModule;
+import edu.wpi.first.wpilibj.AnalogEncoder;
 
 
 
 /** Add your docs here. */
 public class SwerveDriveTrainSubsystem extends SubsystemBase{
 
-    SwerveModuleState frontModuleState ;
-    SwerveModule frontLeftModule = new SwerveModule(3, 2, 3.0 , new Rotation2d(Math.toRadians(45)));
-    SwerveModule frontRightModule = new SwerveModule(5, 4, 3.0, new Rotation2d(Math.toRadians(45)));
-    SwerveModule backLeftModule = new SwerveModule(7, 6, 3 ,new Rotation2d(Math.toRadians(45)));
-    SwerveModule backRightModule = new SwerveModule(9, 8, 3 ,new Rotation2d(Math.toRadians(45)));
+    public static AnalogEncoder frontLeftEncoder = new AnalogEncoder(0);
+    AnalogEncoder frontRightEncoder = new AnalogEncoder(1);
+    AnalogEncoder backLeftEncoder = new AnalogEncoder(3);
+    AnalogEncoder backRightEncoder = new AnalogEncoder(2);
+
+
+
+    SwerveModule frontLeftModule = new SwerveModule(3, 2);
+    SwerveModule frontRightModule = new SwerveModule(5, 4);
+    SwerveModule backLeftModule = new SwerveModule(7, 6);
+    SwerveModule backRightModule = new SwerveModule(8, 9);
+
+    Translation2d frontLeft = new Translation2d((Constants.chasisWidth/2), (Constants.chasisLength/2));
+    Translation2d frontRight = new Translation2d((Constants.chasisWidth/2), (-Constants.chasisLength/2));
+    Translation2d backLeft = new Translation2d((-Constants.chasisWidth/2), (Constants.chasisLength/2));
+    Translation2d backRight = new Translation2d((-Constants.chasisWidth/2), (-Constants.chasisLength/2));
+
+    SwerveDriveKinematics kinematics = new SwerveDriveKinematics(frontLeft, frontRight, backLeft, backRight);
+
+    ChassisSpeeds chassisSpeeds; 
 
     public SwerveDriveTrainSubsystem(){
+        
         double moduleStateLog[]=
         {
             frontLeftModule.getModuleState().angle.getRadians(), // Front Left Module angle
@@ -37,31 +58,74 @@ public class SwerveDriveTrainSubsystem extends SubsystemBase{
             backRightModule.getModuleState().speedMetersPerSecond  // Back Right Module velocity -> m/s
 
         };
-        SmartDashboard.putNumberArray("Setting Initial State Value:",moduleStateLog);
+        // System.out.println("Front left module angle: "+ moduleStateLog[0]);
+        // System.out.println("Front right module angle: "+ moduleStateLog[2]);
+        // System.out.println("Back left module angle: "+ moduleStateLog[4]);
+        // System.out.println("Back right module angle: "+ moduleStateLog[6]);
+
+        // System.out.println();
+
+        // System.out.println("Front left module speed: "+ moduleStateLog[1]);
+        // System.out.println("Front right module speed: "+ moduleStateLog[3]);
+        // System.out.println("Back left module speed: "+ moduleStateLog[5]);
+        // System.out.println("Back right module speed: "+ moduleStateLog[7]);
+
+
+       // SmartDashboard.putNumberArray("Setting Initial State Value:",moduleStateLog);
          
-        setDefaultCommand(new RunCommand(()-> Robot.swerveTrain.driveSwerve(), this ));
+        setDefaultCommand(new RunCommand(()-> Robot.swerveTrain.driveSwerve(Robot.io.driveController), this ));
+        
 
     }
 
-    public void driveSwerve(){
-        System.out.println("Swerve Drive");
-        System.out.println();
+    public void driveSwerve(XboxController drivController){
+        // System.out.println("Swerve Drive");
 
-        // double moduleStateLog[]=
-        // {
-        //     0, // Front Left Module angle
-        //     0, // Front Left Module velocity -> m/s
-        //     0, // Front Right Module angle 
-        //     0, // Front Right Module Velocity -> m/s
-        //     0, // Back Left Module angle
-        //     0, // Back Left Module Velocity -> m/s 
-        //     0, // Back Right Module angle 
-        //     0  // Back Right Module velocity -> m/s
+        double velocityX = -1 * drivController.getLeftY();
+        double VelocityY = drivController.getLeftX();
+        double omega = drivController.getRightX();
 
-        // };
+        if(Math.abs(velocityX) < 0.1) velocityX = 0;
+        if(Math.abs(VelocityY) < 0.1) VelocityY = 0;
+        if(Math.abs(omega) < 0.1) omega = 0;
+   
+
+        // System.out.println("Velocity X: " + velocityX);
+        // System.out.println("Velocity Y: "+VelocityY);
+        // System.out.println("Omega: "+omega);
+
+        chassisSpeeds = new ChassisSpeeds(velocityX, 0, 0);
+
+        // System.out.println("Chasis Speed: "+chassisSpeeds);
+
+        setSpeed(chassisSpeeds);
+
+        frontRightModule.driveMotor.set(0);
+        frontRightModule.steeringMotor.set(0);
+        backLeftModule.driveMotor.set(0);
+        backLeftModule.steeringMotor.set(0);
+        backRightModule.driveMotor.set(0);
+        backRightModule.steeringMotor.set(0);
+
+
+        System.out.println("Front Left Angle: " + frontLeftEncoder.get());
+        // System.out.println("Front Back Angle: " + frontRightEncoder.get());
+        // System.out.println("Back Left Angle: " + backLeftEncoder.get());
+        // System.out.println("Front Left Angle: " + backRightEncoder.get());
+
+
+
+    }
+
+    public void setSpeed(ChassisSpeeds speed){
+        System.out.println("Setting states " );
+        SwerveModuleState states[] = kinematics.toSwerveModuleStates(speed);
+        System.out.println("Setting states "+states );
+        frontLeftModule.setModuleState(states[0]);
+    //     frontRightModule.setModuleState(states[1]);
+    //     backLeftModule.setModuleState(states[2]);
+    //     backRightModule.setModuleState(states[3]);
     }
 
 
-
-    
 }
